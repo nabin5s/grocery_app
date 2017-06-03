35fc1cad33e4512bf3a55a2bdfddbc9ed3fae626
Nabin Shrestha <nabin@email.us>
2017-06-03 16:00:33 +0200

    Added files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var LiveServer = require('gulp-live-server');
+var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
+
+
+gulp.task('live-server', function() {
+	var server = new LiveServer('server/main.js');
+	server.start();
+})
+
+gulp.task('bundle', function() {
+	return browserify({
+		entries:'app/main.jsx',
+		debug:true,
+	})
+	.transform(reactify)
+	.bundle()
+	.pipe(source('app.js'))
+	.pipe(gulp.dest('./.tmp'));
+})
+
+gulp.task('serve', ['live-server'], function() {
+	browserSync.init(null, {
+		proxy:"http://192.168.0.10:3000",
+		port: 9001
+	})
+})
